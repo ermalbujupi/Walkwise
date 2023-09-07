c4809584d5cc760a8dc4edbfdd3f0745b816fa35
Ermal Bujupaj <ermal.bj@gmail.com>
2023-09-07 23:33:28 +0200

    first commit

---
+++ Walkwise/WalkwiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkwiseApp.swift
+//  Walkwise
+//
+//  Created by Ermal Bujupaj on 07.09.23.
+//
+
+import SwiftUI
+
+@main
+struct WalkwiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
